e26baa62ebb6cc123960efc99ca95e651e31fc43
MrLivixx <54632865+MrLivixx@users.noreply.github.com>
2020-02-07 18:41:36 +0300

    :)

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,9 @@
-
+/*
 const DBL = require('dblapi.js');
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3MjI4NTk1MDAzNDQ0NDI5OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTc0MDgyODI0fQ.QvX-4Td26PTfsl9gXO9Y279WK3zIjYiB4Eo9GoGuyUQ');
+const dbl = new DBL(API KEY TOP.GG);
 const SDC = require('sdc-api');
-const client = new SDC('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3MjI4NTk1MDAzNDQ0NDI5OCIsInBlcm1zIjowLCJpYXQiOjE1NzcxMTYxMDJ9.a4-2KNbVvKD_917PMsuS_sSX4JWX-LQO-ElF5HaKkGU')
-const strftime = require('strftime'); // Без него тут никак xd
-const vremya_po_msk = strftime.timezone(180); // окда
+const client = new SDC(API KEY SDC_API)
+*/
 module.exports = async (bot) => {
 bot.user.setStatus("online"); // сам ставишь idle, dbd, invisible или online
 setInterval(function(){
@@ -31,25 +30,12 @@ setInterval(function(){
       bot.user.setActivity(`на ${bot.users.size} участника`,{ type: 'WATCHING'})
       break;
 }
-  },5000);
-      dbl.postStats(bot.guilds.size);
+  },15*1000);
+     // dbl.postStats(bot.guilds.size);
 
     console.log(`Бот работает под ником: ${bot.user.tag}!`);
     bot.generateInvite(['ADMINISTRATOR']).then(link =>{
       console.log('Ссылка на бота:', link);
     })
 
-setInterval(function() {
-bot.channels.get('664961353819095101').setName(`🕥 ${vremya_po_msk('%H:%M', new Date())} по МСК`);
-}, 60000); // Время обновляется каждую минуту
-// Всё!
-client.setAutopost(bot)
-
-
-bot.generateInvite(["ADMINISTRATOR"]).then(link => {
-console.log(link);
-});
-async function test1() {
-bot.channels.find(c => c.id === "664961588029292554").setName(`В сети: ${bot.guilds.get('502949305372377096').presences.size}`);
-}; setInterval(test1, 30000)
-}
+//client.setAutopost(bot)
